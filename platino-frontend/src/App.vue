268cb3b5e6b4a5f4f552186deb801c8918dbcb71
Jeyker Salinas <jeyker.salinas13@gmail.com>
2025-06-19 21:44:19 +0200

    first commit, vue project created

---
+++ platino-frontend/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <router-view />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+let num = 0
+num = 'a'  //
+</script>
---
+++ platino-frontend/src/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <HelloWorld />
+  <Hol></Hol>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
